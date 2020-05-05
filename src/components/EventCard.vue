e2a302183a815b6d8631e30643bde4a883660986
Wallace <wllcblm@gmail.com>
2020-05-05 19:04:15 -0300

    Dynamic routes & History mode

---
+++ src/components/EventCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    <div>
+      <ul>
+        <li></li>
+      </ul>
+    </div>
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style></style>
